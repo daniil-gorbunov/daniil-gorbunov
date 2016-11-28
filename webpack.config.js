8631662eda1b578b312454aecac4571c4d752d65
daniil-gorbunov <daniil_harbunou@epam.com>
2016-11-29 00:00:26 +0300

    config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-'use strict';
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
 
 module.exports = {
     entry: './src/index',
@@ -6,12 +7,21 @@ module.exports = {
         path: __dirname + '/scripts',
         filename: 'app.js'
     },
-    watch: true,
+    watch: NODE_ENV == 'development',
 
     watchOptions: {
         aggregateTimeout: 100
     },
 
-    devtool: 'source-map',
+    devtool: NODE_ENV == 'development' ? 'source-map' : null,
+    
+    plugins: [
+        new webpack.EnvironmentPlugin('NODE_ENV')
+    ],
+
+    loaders: [{
+        test: /\.js$/,
+        loader: 'babel-loader?optional[]=runtime&stage=0'
+    }]
 
 };
